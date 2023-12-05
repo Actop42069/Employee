8ab076f4141f1c87b6756a0caa8e51fae00ff7ca
Achyut <agaihr10@gmail.com>
2023-12-05 13:30:48 +0545

    Completed the read part of the model using manaual entry by SQL Server

--- EmployeeData/Controllers/EmployeeController.cs
+++ EmployeeData/Controllers/EmployeeController.cs
@@ -1,12 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EmployeeData.DAL;
+using EmployeeData.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeData.Controllers
 {
     public class EmployeeController : Controller
     {
+        private readonly EmployeeDbContext _context;
+        public EmployeeController(EmployeeDbContext context)
+        {
+            this._context = context;
+        }
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            var employees = _context.Employees.ToList();
+			List<EmployeeViewModelcs> employeeList = new List<EmployeeViewModelcs>();
+
+			if (employees != null)
+            {
+                foreach(var employee in employees)
+                {
+                    var EmployeeViewModelcs = new EmployeeViewModelcs()
+                    {
+                        Id = employee.Id,
+                        FirstName = employee.FirstName,
+                        LastName = employee.LastName,
+                        Email = employee.Email,
+                        Salary = employee.Salary
+                    };
+                    employeeList.Add(EmployeeViewModelcs);
+                }
+                return View(employeeList);
+            }
+            return View(employeeList);
         }
     }
 }
--- EmployeeData/Models/EmployeeViewModelcs.cs
+++ EmployeeData/Models/EmployeeViewModelcs.cs
@@ -1,6 +1,25 @@
-﻿namespace EmployeeData.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeData.Models
 {
     public class EmployeeViewModelcs
     {
+        public int Id { get; set; }
+        
+        public string FirstName { get; set; }
+        [DisplayName("First Name")]
+        public string LastName { get; set; }
+        [DisplayName("Last Name")]
+        public DateTime DateOfBirth { get; set; }
+        [DisplayName("Date Of Birth")]
+        public string Email { get; set; }
+        public double Salary { get; set; }
+
+        [DisplayName("Name")]
+        public string FullName {
+            get { return FirstName + " " + LastName; }
+        }
+        
     }
 }
